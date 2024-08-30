2c6b6f814d064fcda255f22c074fb4be6b4be23c
Emrecan Pürçek <canpurcek@gmail.com>
2024-08-30 19:03:58 +0300

    delete-note

--- lib/notebook_screen.dart
+++ lib/notebook_screen.dart
@@ -6,6 +6,9 @@ import 'dart:convert';
 
 import 'package:notebook_app/edit_note_screen.dart';
 
+
+enum SampleItem { itemOne, itemTwo, itemThree }
+
 class NotebookScreen extends StatefulWidget {
   const NotebookScreen({super.key});
 
@@ -42,6 +45,27 @@ class _NotebookScreenState extends State<NotebookScreen> {
     }
   }
 
+  Future<void> deleteNote(BuildContext context, String id) async {
+  final response = await http.post(
+    Uri.parse('https://emrecanpurcek.com.tr/notebook/delete.php'),
+    body: {'id': id},
+  );
+
+  final data = json.decode(response.body);
+
+  if (data['success'] == 1) {
+    // Başarılı silme işlemi
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Note deleted successfully')),
+    );
+  } else {
+    // Hata mesajı
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Error: ${data['message']}')),
+    );
+  }
+}
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -60,29 +84,31 @@ class _NotebookScreenState extends State<NotebookScreen> {
   }
 
   Widget masonryView(BuildContext context) {
-    return MasonryGridView.builder(
-        gridDelegate:
-            SliverSimpleGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-        itemCount: _notes.length,
-        itemBuilder: (context, index) {
-          final note = _notes[index];
-          return GestureDetector(
-            onTap: () async {
-              final result = await Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => EditNoteScreen(note: note),
-                ),
-              );
-              if (result == true) {
-                _fetchNotes(); // Eğer not başarıyla güncellendiyse, notları yeniden çek
-              }
-            },
-            child: Card(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+  return MasonryGridView.builder(
+    gridDelegate: SliverSimpleGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+    itemCount: _notes.length,
+    itemBuilder: (context, index) {
+      final note = _notes[index];
+      return GestureDetector(
+        onTap: () async {
+          final result = await Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => EditNoteScreen(note: note),
+            ),
+          );
+          if (result == true) {
+            _fetchNotes(); // Eğer not başarıyla güncellendiyse, notları yeniden çek
+          }
+        },
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween, // Bu satırı ekleyin
                   children: [
                     Text(
                       note['title'],
@@ -91,26 +117,50 @@ class _NotebookScreenState extends State<NotebookScreen> {
                         color: Colors.black,
                       ),
                     ),
-                    SizedBox(height: 8.0),
-                    Container(
-                      alignment: Alignment.topLeft,
-                      child: Text(
-                        note['note'],
-                        style: TextStyle(color: Colors.black),
-                      ),
+                    PopupMenuButton(
+                      onSelected: (value) async {
+                        if (value == SampleItem.itemOne) {
+                          // Silme işlemi
+                          await deleteNote(context, note['id']);
+
+                          // Ekranı güncellemek için setState ile _fetchNotes çağrısı
+                          setState(() {
+                            _fetchNotes();
+                          });
+                        }
+                      },
+                      itemBuilder: (BuildContext context) => <PopupMenuEntry<SampleItem>>[
+                        const PopupMenuItem<SampleItem>(
+                          value: SampleItem.itemOne,
+                          child: Text('Delete'),
+                        ),
+                      ],
                     ),
-                    SizedBox(height: 8.0),
-                    Text(
-                      note['date'],
-                      style: TextStyle(color: Colors.black),
-                    ),
+
                   ],
                 ),
-              ),
+                SizedBox(height: 8.0),
+                Container(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    note['note'],
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                Text(
+                  note['date'],
+                  style: TextStyle(color: Colors.black),
+                ),
+              ],
             ),
-          );
-        });
-  }
+          ),
+        ),
+      );
+    },
+  );
+}
+
 
   Widget floating(BuildContext context) {
     return FloatingActionButton(
