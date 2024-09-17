6cdc3aec9b7978d76869a95d6f3b4a8e8bcc574f
canpurcek34 <canpurcek@gmail.com>
2024-09-17 16:51:00 +0300

    ui-improvements(texfields, edit date and id containers)

--- lib/ui/add_note_screen.dart
+++ lib/ui/add_note_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-
 class AddNoteScreen extends StatefulWidget {
   @override
   _AddNoteScreenState createState() => _AddNoteScreenState();
@@ -12,19 +11,15 @@ class _AddNoteScreenState extends State<AddNoteScreen> {
   final _formKey = GlobalKey<FormState>();
   String _title = '';
   String _note = '';
-  
 
   @override
   void initState() {
     super.initState();
   }
 
-  
-  final _date = DateTime.now();
+  final _date = DateTime.now(); //tarih formatı düzenlenecek
 
   Future<void> addNote() async {
-    
-
     final response = await http.post(
       Uri.parse('https://emrecanpurcek.com.tr/notebook/insert.php'),
       headers: <String, String>{
@@ -62,35 +57,74 @@ class _AddNoteScreenState extends State<AddNoteScreen> {
           key: _formKey,
           child: Column(
             children: [
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Başlık',
-                  border: InputBorder.none
+              Container( //gölgelendirme için gerekli container
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.8),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: Offset(0, 3), // Gölgenin konumu
+                    ),
+                  ],
+                  //border: Border.all(color: Colors.grey), //textfield kenarlığı
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Başlık',
+                      border: InputBorder.none,
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Başlık giriniz';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _title = value!;
+                    },
                   ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Başlık giriniz';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _title = value!;
-                },
+                ),
               ),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Not',
-                  border: InputBorder.none
+              SizedBox(height: 16),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                  //border: Border.all(color: Colors.grey), //texfield kenarlığı
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Not',
+                      border: InputBorder.none,
+                    ),
+                    maxLines: null, // Kullanıcı istediği kadar satır ekleyebilir
+                    keyboardType: TextInputType.multiline, // Çok satırlı metin girişi sağlar
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Not giriniz';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _note = value!;
+                    },
                   ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Not giriniz';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _note = value!;
-                },
+                ),
               ),
               SizedBox(height: 20),
               ElevatedButton(
--- lib/ui/edit_note_screen.dart
+++ lib/ui/edit_note_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import 'package:intl/intl.dart';
+
 class EditNoteScreen extends StatefulWidget {
   final Map<String, dynamic> note;
 
@@ -21,12 +23,10 @@ class _EditNoteScreenState extends State<EditNoteScreen> {
     super.initState();
     _titleController.text = widget.note['title'] ?? '';
     _noteController.text = widget.note['note'] ?? '';
-    _date = widget.note['date'] ?? DateTime.now().toString();
+    _date = widget.note['date'] ?? DateFormat.yMMMMd('en_US').add_jm(); //tarih formatı düzenlenecek
   }
 
-
   Future<void> _updateNote() async {
-
     final response = await http.post(
       Uri.parse('https://emrecanpurcek.com.tr/notebook/update.php'),
       headers: <String, String>{
@@ -53,14 +53,8 @@ class _EditNoteScreenState extends State<EditNoteScreen> {
         SnackBar(content: Text('Hata: ${data['message']}')),
       );
       print("Veri güncellenmesi başarısız");
-      print("----------------------------");
-      print(widget.note['id']);
-      print(_titleController.text);
-      print(_noteController.text);
-      print(_date);
     }
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -81,52 +75,138 @@ class _EditNoteScreenState extends State<EditNoteScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextField(
-                controller: _titleController,
-                decoration: InputDecoration(
-                  labelText: 'Title',
-                  border: InputBorder.none,
-                  labelStyle: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.black
-                  )
-                  ),                  
-              ),
-              TextField(
-                controller: _noteController,
-                decoration: InputDecoration(
-                  labelText: 'Note',
-                  border: InputBorder.none,
-                  labelStyle: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.black
-                  )
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Title TextField
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: Offset(0, 3), // Gölgenin konumu
                   ),
-                maxLines: 6,
+                ],
               ),
-              SizedBox(height: 16),
-              Text(
-                'Düzenlenme zamanı: $_date', // Salt okunur tarih
-                style: TextStyle(fontSize: 16,),
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: TextField(
+                  controller: _titleController,
+                  decoration: InputDecoration(
+                    labelText: 'Title',
+                    border: InputBorder.none,
+                    labelStyle: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
               ),
-              SizedBox(height: 16),
-              Text(
-                'id: $id ', // Salt okunur id
-                style: TextStyle(fontSize: 16,),
+            ),
+            SizedBox(height: 16),
+            // Note TextField
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: Offset(0, 3),
+                  ),
+                ],
               ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  _updateNote();
-                },
-                child: Text('Kaydet'),
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: TextField(
+                  controller: _noteController,
+                  decoration: InputDecoration(
+                    labelText: 'Note',
+                    border: InputBorder.none,
+                    labelStyle: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.black,
+                    ),
+                  ),
+                  minLines: 1, // Minimum satır sayısı
+                  maxLines: null, // Metin büyüdükçe yüksekliği artırır
+                  expands: false, // Dinamik genişlemeyi sağlar
+                ),
               ),
-            ],
-          ),
+            ),
+            SizedBox(height: 16),
+            // Date and ID in a Row
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // Date Container
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                      child: Text(
+                        'Düzenlenme zamanı: $_date',
+                        style: TextStyle(fontSize: 16, color: Colors.black),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 16),
+                // ID Container
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                      child: Text(
+                        'id: $id',
+                        style: TextStyle(fontSize: 16, color: Colors.black),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            // Save Button
+            ElevatedButton(
+              onPressed: () {
+                _updateNote();
+              },
+              child: Text('Kaydet'),
+            ),
+          ],
         ),
+      ),
     );
   }
 }
